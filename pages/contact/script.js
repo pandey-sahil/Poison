08148e2c7c52d1b12f94634f3db1299e2c66e5ee
pandey-sahil <sahilp2593@gmail.com>
2025-01-07 19:17:26 +0530

    added drag ball in contact page

---
+++ pages/contact/script.js
@@ -0,0 +1,67 @@
+function ballDrag() {
+    const container = document.querySelector(".contact-hero");
+    const dragBalls = document.querySelectorAll(".contact-drag-ball");
+  
+    let velocity = { x: 0, y: 0 };
+    let lastPosition = { x: 0, y: 0 };
+  
+    function updateDraggable() {
+      const bounds = isMobile()
+        ? {
+            top: 0,
+            left: 0,
+            width: window.innerWidth * 0.8,
+            height: window.innerHeight * 0.5,
+          }
+        : container;
+  
+        dragBalls.forEach(ball => {
+          Draggable.create(ball, {
+            type: "x,y",
+            bounds: bounds,
+            inertia: true,
+            onDragStart: function () {
+              gsap.killTweensOf(ball);
+              velocity = { x: 0, y: 0 };
+            },
+            onDrag: function () {
+              const currentPosition = { x: this.x, y: this.y };
+              velocity = {
+                x: currentPosition.x - lastPosition.x,
+                y: currentPosition.y - lastPosition.y,
+              };
+              lastPosition = currentPosition;
+            },
+            onDragEnd: function () {
+              const multiplier = isMobile() ? 5 : 10;
+              gsap.to(ball, {
+                x: `+=${velocity.x * multiplier}`,
+                y: `+=${velocity.y * multiplier}`,
+                duration: isMobile() ? 0.5 : 1,
+                ease: "elastic.out(0.4, 0.2)",
+              });
+            },
+          });
+          
+        });
+
+    }
+  
+    updateDraggable();
+  
+    window.addEventListener("scroll", () => {
+      const scrollOffset = window.scrollY;
+      const currentPosition = ball.getBoundingClientRect();
+      const containerBounds = container.getBoundingClientRect();
+  
+      const newLeft = currentPosition.left - containerBounds.left;
+      const newTop = currentPosition.top - containerBounds.top + scrollOffset;
+  
+      gsap.set(ball, { x: newLeft, y: newTop });
+    });
+  
+    window.addEventListener("resize", updateDraggable);
+  }
+  
+
+  ballDrag()
